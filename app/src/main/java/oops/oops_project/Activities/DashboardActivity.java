ef1770dbd9d7e81e2fe943a63dc43549f793eb22
Nandan H R <nandan.hr006@gmail.com>
2020-11-08 14:20:08 +0530

    Set up dashboard functionality and improved bottom navigation
    - Fix Bottom Navigation backbutton bug

--- app/src/main/java/oops/oops_project/Activities/DashboardActivity.java
+++ app/src/main/java/oops/oops_project/Activities/DashboardActivity.java
@@ -3,19 +3,30 @@ package oops.oops_project.Activities;
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import oops.oops_project.R;
 
 public class DashboardActivity extends AppCompatActivity
 {
+    private int id;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
-        //Temporary code
+    }
+
+    private void sendIntent()
+    {
         Intent intent = new Intent(this, NavigatingDashboardActivity.class);
+        intent.putExtra(NavigatingDashboardActivity.CHOICE, id);
         startActivity(intent);
     }
+
+    public void choseInventory(View view) { id = R.id.bnav_inventory; sendIntent();}
+    public void choseTasks(View view) { id = R.id.bnav_tasks; sendIntent();}
+    public void choseNotes(View view) { id = R.id.bnav_notes; sendIntent();}
+    public void choseDiary(View view) { id = R.id.bnav_diary; sendIntent();}
 }
--- app/src/main/java/oops/oops_project/Activities/NavigatingDashboardActivity.java
+++ app/src/main/java/oops/oops_project/Activities/NavigatingDashboardActivity.java
@@ -8,15 +8,19 @@ import android.os.Bundle;
 import android.view.MenuItem;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import java.util.Stack;
 
 import oops.oops_project.Fragments.*;
 import oops.oops_project.R;
 
 public class NavigatingDashboardActivity extends AppCompatActivity
 {
-    private Fragment fragment = null;
     private String cur_category = "";
+    private BottomNavigationView bottomNav;
     private FloatingActionButton fab;
+    private Listener listener;
+    private final Stack<BnavItem> bnavBackStack = new Stack<>();
+    public static final String CHOICE = "choice";
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -25,9 +29,46 @@ public class NavigatingDashboardActivity extends AppCompatActivity
         setContentView(R.layout.activity_navigating_dashboard);
 
         fab = findViewById(R.id.fab);
-        BottomNavigationView bottomNav = findViewById(R.id.bottom_nav);
-        Listener listener = new Listener();
+        bottomNav = findViewById(R.id.bottom_nav);
+
+        int id = getIntent().getIntExtra(CHOICE, R.id.bnav_inventory);
+        setFragment(id, true);
+
+        listener = new Listener();
         bottomNav.setOnNavigationItemSelectedListener(listener);
+    }
+
+    private void setFragment(int id, boolean firstTime)
+    {
+        String category;
+        int fabImageResource;
+        Fragment fragment;
+
+        if(id == R.id.bnav_inventory) { category = "Inventory"; fragment = new InventoryFragment(); fabImageResource = R.drawable.baseline_add_24;}
+        else if(id == R.id.bnav_tasks) { category = "Tasks"; fragment = new TasksFragment(); fabImageResource = R.drawable.baseline_add_task_24;}
+        else if(id == R.id.bnav_notes) { category = "Notes"; fragment = new NotesFragment(); fabImageResource = R.drawable.baseline_note_add_white_24;}
+        else { category = "Diary"; fragment = new DiaryFragment(); fabImageResource = R.drawable.baseline_create_24;}
+
+        if(!category.equals(cur_category))
+        {
+
+            fab.setImageResource(fabImageResource);
+
+            FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+            if(cur_category.equals(""))
+                ft.add(R.id.fragment_container, fragment);
+            else
+                ft.replace(R.id.fragment_container, fragment);
+            if(!firstTime)
+                ft.addToBackStack(null);
+            else
+                bottomNav.setSelectedItemId(id);
+            ft.commit();
+
+            bnavBackStack.push(new BnavItem(id, fabImageResource));
+
+            cur_category = category;
+        }
     }
 
     public class Listener implements BottomNavigationView.OnNavigationItemSelectedListener
@@ -35,31 +76,34 @@ public class NavigatingDashboardActivity extends AppCompatActivity
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item)
         {
-            int id = item.getItemId(), fabImageResource;
-            String category;
-
-            if(id == R.id.bnav_inventory) { category = "Inventory"; fragment = new InventoryFragment(); fabImageResource = R.drawable.baseline_add_24;}
-            else if(id == R.id.bnav_tasks) { category = "Tasks"; fragment = new TasksFragment(); fabImageResource = R.drawable.baseline_add_task_24;}
-            else if(id == R.id.bnav_notes) { category = "Notes"; fragment = new NotesFragment(); fabImageResource = R.drawable.baseline_note_add_white_24;}
-            else if(id == R.id.bnav_diary) { category = "Diary"; fragment = new DiaryFragment(); fabImageResource = R.drawable.baseline_create_24;}
-            else { category = ""; fragment = null; fabImageResource = 0;}
-
-            if(!category.equals(cur_category))
-            {
-                fab.setImageResource(fabImageResource);
-
-                FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
-                if(cur_category.equals(""))
-                    ft.add(R.id.fragment_container, fragment);
-                else
-                    ft.replace(R.id.fragment_container, fragment);
-                ft.addToBackStack(null);
-                ft.commit();
-
-                cur_category = category;
-            }
-
+            int id = item.getItemId();
+            setFragment(id, false);
             return true;
+        }
+    }
+
+    @Override
+    public void onBackPressed()
+    {
+        if(bnavBackStack.size() > 1)
+        {
+            bottomNav.setOnNavigationItemSelectedListener(null);
+            bnavBackStack.pop();
+            bottomNav.setSelectedItemId(bnavBackStack.peek().item);
+            fab.setImageResource(bnavBackStack.peek().fabImage);
+            bottomNav.setOnNavigationItemSelectedListener(listener);
+        }
+        super.onBackPressed();
+    }
+
+    private static class BnavItem
+    {
+        int item, fabImage;
+
+        BnavItem(int item, int fabImage)
+        {
+            this.fabImage = fabImage;
+            this.item = item;
         }
     }
 }
