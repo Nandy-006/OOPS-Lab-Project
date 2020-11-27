14f15652b1e4195ce0c4664ddc04dd1eb0bccafc
Sathvik Bhaskarpandit <bsathvik007@gmail.com>
2020-11-27 20:54:03 +0530

    Almost done with auth

--- app/src/main/java/oops/oops_project/Activities/MainActivity.java
+++ app/src/main/java/oops/oops_project/Activities/MainActivity.java
@@ -1,53 +1,78 @@
 package oops.oops_project.Activities;
 
 import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.app.AppCompatDelegate;
 
+import oops.oops_project.AuthActivities.AnonymousAuthActivity;
+import oops.oops_project.AuthActivities.EmailPasswordActivity;
+import oops.oops_project.AuthActivities.FacebookLoginActivity;
+import oops.oops_project.AuthActivities.GoogleSignInActivity;
+import oops.oops_project.AuthActivities.ManageUserActivity;
+import oops.oops_project.AuthActivities.PhoneAuthActivity;
+import oops.oops_project.R;
+import androidx.appcompat.app.AppCompatActivity;
+
+
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Adapter;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.firebase.ui.auth.AuthUI;
 import com.firebase.ui.auth.IdpResponse;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseException;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
+import com.google.firebase.auth.PhoneAuthCredential;
+
+import com.google.firebase.auth.PhoneAuthOptions;
+import com.google.firebase.auth.PhoneAuthProvider;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
-import oops.oops_project.R;
-
-public class MainActivity extends AppCompatActivity
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener
 {
+    static boolean loginClicked;
+    // tdfsgsfdgsfgjshfljfs
+
+    /*FirebaseAuth mAuth;
+
     private static final int RC_SIGN_IN = 123;
-    FirebaseAuth mAuth;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
-
-        super.onCreate(savedInstanceState);
-        mAuth = FirebaseAuth.getInstance();
-        createSignInIntent();
-        setContentView(R.layout.activity_main);
-        signOut();
-
-    }
-
 
 
     public void createSignInIntent() {
         // [START auth_fui_create_intent]
         // Choose authentication providers
-        //AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+
         List<AuthUI.IdpConfig> providers = Arrays.asList(
                 new AuthUI.IdpConfig.EmailBuilder().build(),
                 new AuthUI.IdpConfig.PhoneBuilder().build(),
-                new AuthUI.IdpConfig.GoogleBuilder().build(),
-                new AuthUI.IdpConfig.FacebookBuilder().build(),
-                new AuthUI.IdpConfig.TwitterBuilder().build());
+                new AuthUI.IdpConfig.GoogleBuilder().build());
+
 
         // Create and launch sign-in intent
         startActivityForResult(
@@ -57,36 +82,82 @@ public class MainActivity extends AppCompatActivity
                         .build(),
                 RC_SIGN_IN);
         // [END auth_fui_create_intent]
+
+    }
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mAuth = FirebaseAuth.getInstance();
+        createSignInIntent();
+        setContentView(R.layout.activity_main);
+        Intent launchSecondActiivity = new Intent(this, UserDetailsEntry.class);
+        startActivity(launchSecondActiivity);
+
+
+
+    }
+
+
+     */
+
+    private static final Class[] CLASSES = new Class[]{
+            EmailPasswordActivity.class,
+            GoogleSignInActivity.class,
+            //FacebookLoginActivity.class,
+           // AnonymousAuthActivity.class,
+            //PhoneAuthActivity.class,
+            ManageUserActivity.class
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Intent intent = getIntent();
+        loginClicked = intent.getBooleanExtra("LoginClicked", true);
+        //Toast.makeText(getApplicationContext(), Boolean.toString(loginClicked), Toast.LENGTH_SHORT).show();
+
+        ListView listView = findViewById(R.id.list_view);
+
+        MyArrayAdapter adapter = new MyArrayAdapter(this, android.R.layout.simple_list_item_1, CLASSES);
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(this);
+
     }
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        Class clicked = CLASSES[position];
+        Intent intent = new Intent(this, clicked);
+        intent.putExtra("LoginClicked", loginClicked);
+        startActivity(intent);
+    }
 
-        if (requestCode == RC_SIGN_IN) {
-            IdpResponse response = IdpResponse.fromResultIntent(data);
+    private static class MyArrayAdapter extends ArrayAdapter<Class> {
+        private Context mContext;
+        private Class[] mClasses;
 
-            if (resultCode == RESULT_OK) {
-                // Successfully signed in
-                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-                // ...
-            } else {
-                response.getError().getErrorCode();
-                // Sign in failed. If response is null the user canceled the
+        private MyArrayAdapter(Context context, int resource, Class[] objects) {
+            super(context, resource, objects);
+            mContext = context;
+            mClasses = objects;
+        }
 
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            View view = convertView;
+
+            if (convertView == null) {
+                LayoutInflater inflater = (LayoutInflater) mContext.getSystemService(LAYOUT_INFLATER_SERVICE);
+                view = inflater.inflate(android.R.layout.simple_list_item_1, null);
             }
+            ((TextView) view.findViewById(android.R.id.text1)).setText(mClasses[position].getSimpleName());
+
+            return view;
         }
     }
 
-    public void signOut() {
-        // [START auth_fui_signout]
-        AuthUI.getInstance()
-                .signOut(this)
-                .addOnCompleteListener(new OnCompleteListener<Void>() {
-                    public void onComplete(@NonNull Task<Void> task) {
-                        // ...
-                    }
-                });
-        // [END auth_fui_signout]
-    }
 }
