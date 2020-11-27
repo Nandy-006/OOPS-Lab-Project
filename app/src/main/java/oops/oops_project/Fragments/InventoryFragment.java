e8d3f96d6f000733f5b608dce016d2b22dc73786
Chandra Vamsi <vamsi.tvcv901@gmail.com>
2020-11-27 19:16:24 +0530

    Need to implement Add Item

--- app/src/main/java/oops/oops_project/Fragments/InventoryFragment.java
+++ app/src/main/java/oops/oops_project/Fragments/InventoryFragment.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.ItemTouchHelper;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -12,20 +13,14 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import java.util.ArrayList;
-
-import oops.oops_project.Activities.AddCategoryDialog;
-import oops.oops_project.Activities.Category;
 import oops.oops_project.Activities.CategoryAdapter;
 import oops.oops_project.Database.Inventory;
 import oops.oops_project.R;
 
-public class InventoryFragment extends Fragment
-{
+public class InventoryFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
     {
-
         return inflater.inflate(R.layout.fragment_inventory, container, false);
     }
 
